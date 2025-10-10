f4d055728bc478d0608af5ced63afaadac2d219a
Divino Silva <fernandinho_07@hotmail.com>
2025-10-10 13:49:53 -0300

    test(health): add request spec for /healthz

--- spec/requests/health_spec.rb
+++ spec/requests/health_spec.rb
@@ -4,8 +4,6 @@ RSpec.describe "Health", type: :request do
   it "returns ok" do
     get "/healthz"
     expect(response).to have_http_status(:ok)
-    json = JSON.parse(response.body)
-    expect(json).to include("ok" => true)
-    expect(json).to have_key("time")
+    expect(JSON.parse(response.body)).to include("ok" => true)
   end
 end
